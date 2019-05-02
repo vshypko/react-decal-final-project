dc3207bd6c56f7b1471399bdd23a1e952a19442f
vitali <vshypko@gmail.com>
2019-05-01 22:16:45 -0700

    minor styling changes

--- src/SongDisplay.js
+++ src/SongDisplay.js
@@ -3,7 +3,8 @@ import React from "react";
 class SongDisplay extends React.Component {
     render() {
         return (
-            <div className="ui answer-item" key={this.props.song.id} onClick={() => this.props.checkAnswer(this.props.song)}>
+            <div className="ui answer-item" key={this.props.song.id}
+                 onClick={() => this.props.checkAnswer(this.props.song)}>
                 <div className="ui container">
                     {
                         this.props.song.album.images[0] !== undefined &&
